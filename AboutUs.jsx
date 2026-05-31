3cfafadc6d387e7bf8165a6aad8ebac537bf056c
Benedict Gabriel C <benedictcgabriel@gmail.com>
2026-05-31 14:46:49 +0530

    Add AboutUs component with company information

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,1 +1,21 @@
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
 
+      <p>
+        Brief introduction about the company.
+      </p>
+
+      <p>
+        Mission statement and commitment to quality plants.
+      </p>
+
+      <p>
+        Information about customer satisfaction and sustainability.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
