840fe84a7bf24f0acf9cadc4bbc263dc46de8732
Benedict Gabriel C <benedictcgabriel@gmail.com>
2026-05-31 14:47:25 +0530

    Add landing page component to App

--- App.jsx
+++ App.jsx
@@ -1,1 +1,15 @@
+function App() {
+  return (
+    <div className="landing-page">
+      <h1 className="company-name">
+        Paradise Nursery
+      </h1>
 
+      <button className="get-started-btn">
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default App;
